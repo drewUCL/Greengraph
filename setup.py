f94466f43b8bd389c971f276b43bd536e2ee17a6
Andrew Mann <drewmann47@yahoo.co.uk>
2015-11-10 19:52:20 +0000

    adding project to github

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Greengraph",
+    version = "0.1",
+    packages = find_packages(exclude=['*test']),
+    scripts = ['scripts/graph'],
+    install_requires = ['argparse']
+)
+
+#NOTE: Not sure if scripts/graph is correct
