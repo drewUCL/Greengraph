112ab7cede8099bd34d02553746457f87f831f9b
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-06 13:12:01 +0000

    Updated this file to link to the main init command line from console_scripts

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
 	author = "Andrew D. Mann",
 	author_email = "Andrew.Mann.15@ucl.ac.uk",
     version = "0.1",
+	license = 'MIT',
 	description = "This package will graph the quantity of green pixles relating to green land from one location to another",
     packages = find_packages(exclude=['*test']),
     install_requires = ['argparse','numpy','requests','geopy','matplotlib'],
 	entry_points = {
 		'console_scripts': [ 
-			'greengraph = greengraph:main',
+			'Greengraph = Greengraph:main',
 		],
 	},
-	license = 'MIT',
 )
