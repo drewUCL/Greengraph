ad61706feb41697485d71d6c05d5e91a9394dc63
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-06 14:34:25 +0000

    Added comments to this file, formatted in a more readable layout. The results file will save wherever the user calls the programme from

--- Greengraph/__init__.py
+++ Greengraph/__init__.py
@@ -3,11 +3,19 @@ from argparse import ArgumentParser
 from matplotlib import pyplot as this_plot
 
 def main():
+   ''' 
+       Command Line Interface Allowing The User To Specify Arguments 
+       This Function Then Plots The Results Where The Package Was Called From
+	   For Example, If It Was Called From The Desktop, The Resulting Chart Would Be Saved On The Desktop For The User.
+   '''
+   
    parser = ArgumentParser(description = "Generate Green Density Chart Based on Two Points")
-   parser.add_argument('--From', '-f',help='Enter a start location here such as London', dest='startPoint')
-   parser.add_argument('--To', '-t',help='Enter an end location here such as Oxford', dest='endPoint') 
-   parser.add_argument('--Steps', '-s', type=int, help='Enter the number of steps you wish to chart', default=10)
-   parser.add_argument('--Out', '-o', help='Enter the file name you wish to call the image (Default=PNG)', default='output.png')
+   
+   parser.add_argument('--from', '-f',help='Enter a start location here such as London', dest='startPoint')
+   parser.add_argument('--to', '-t',help='Enter an end location here such as Oxford', dest='endPoint') 
+   parser.add_argument('--steps', '-s', type=int, help='Enter the number of steps you wish to chart', default=10)
+   parser.add_argument('--out', '-o', help='Enter the file name you wish to call the image (Default=PNG)', default='output.png')
+   
    arguments= parser.parse_args()
    
    this_chart = Greengraph(arguments.startPoint,arguments.endPoint)
