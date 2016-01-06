a41c665081149d82f7dbe636aa02b74f6162d595
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-06 17:32:42 +0000

    Changing file name to start with test so nose works

---
+++ Greengraph/test/test_map.py
@@ -0,0 +1,20 @@
+from nose.tools import assert_almost_equal, assert_in, assert_equal, assert_false, assert_true
+from ..Greengraph import geolocate, maps_url_for, get_map_at
+from ..Greengraph import count_green, is_green, location_sequence
+from ..graph import Greengraph
+from ..map import Map
+import png
+import os
+
+#Test the Greengraph class
+def t_Map():
+	''' A function to test the initialisation of Map '''
+	
+def t_green():
+	''' [INSERT COMMENT] '''
+	
+def t_count_green():
+	''' [INSERT COMMENT] '''
+	
+def t_show_green():
+	''' [INSERT COMMENT] '''
