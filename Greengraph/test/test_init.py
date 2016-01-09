d878f887d3aa4b95aadbc136b3dd948f5992b52e
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-09 18:18:27 +0000

    Added a test for users command line initiation

---
+++ Greengraph/test/test_init.py
@@ -0,0 +1,13 @@
+from ..__init__ import process, parser
+from nose.tools import assert_equal
+
+def test_command():
+	'''
+	Description: A function to test the command line arguments
+	Data: STATIC
+	'''
+	commands = parser.parse_args(['--from','London','--to','Edinburgh','--steps',12,'--out','LDN_EDN.png'])
+	assert_equal(commands.StartLocation,'London')
+	assert_equal(commands.EndLocation,'London')
+	assert_equal(commands.steps,12)
+	assert_equal(commands.out,'LDN_EDN.png')
