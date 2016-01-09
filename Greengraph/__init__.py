b2bebcfe3d0a2cc93bf9355e773caaf747228393
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-09 13:03:43 +0000

    Added required argumnets

--- Greengraph/__init__.py
+++ Greengraph/__init__.py
@@ -11,8 +11,8 @@ def process():
    
    parser = ArgumentParser(description = "Generate Green Density Chart Based on Two Points")
    
-   parser.add_argument('--from', '-f',help='Enter a start location here such as London', dest='startPoint')
-   parser.add_argument('--to', '-t',help='Enter an end location here such as Oxford', dest='endPoint') 
+   parser.add_argument('--from', '-f',help='Enter a start location here such as London', dest='startPoint', required=True)
+   parser.add_argument('--to', '-t',help='Enter an end location here such as Oxford', dest='endPoint',required=True) 
    parser.add_argument('--steps', '-s', type=int, help='Enter the number of steps you wish to chart', default=10)
    parser.add_argument('--out', '-o', help='Enter the file name you wish to call the image (Default=PNG)', default='output.png')
    
