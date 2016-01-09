a46b1b5e08d2beffa2f3116e9af113d3611d467f
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-09 13:19:31 +0000

    Changed function structure of charting from command line

--- Greengraph/__init__.py
+++ Greengraph/__init__.py
@@ -2,6 +2,18 @@ from graph import Greengraph
 from argparse import ArgumentParser
 from matplotlib import pyplot as this_plot
 
+def chart(start, end, steps, out):
+   this_chart = Greengraph(start,end)
+   this_data = this_chart.green_between(steps)
+   this_plot.plot(this_data)
+   #Add Annotations
+   this_plot.xlabel("Steps")
+   this_plot.ylabel("'Green Pixel Density'")
+   this_plot.title("Green Land Density: " + start + " - " + end)
+   #Save result and show user
+   this_plot.savefig(out)
+   this_plot.show()
+
 def process():
    ''' 
        Command Line Interface Allowing The User To Specify Arguments 
@@ -11,20 +23,15 @@ def process():
    
    parser = ArgumentParser(description = "Generate Green Density Chart Based on Two Points")
    
-   parser.add_argument('from', '-f',help='Enter a start location here such as London. [STRING]', dest='startPoint', required=True)
-   parser.add_argument('to', '-t',help='Enter an end location here such as Oxford. [STRING]', dest='endPoint',required=True) 
+   #Positional Arguments
+   parser.add_argument('from', help='Enter a start location here such as London. [STRING]', dest='startPoint', required=True)
+   parser.add_argument('to',help='Enter an end location here such as Oxford. [STRING]', dest='endPoint',required=True) 
+   
+   #Optional Arguments
    parser.add_argument('--steps', '-s', type=int, help='Enter the number of steps you wish to chart. [INT]', default=10)
    parser.add_argument('--out', '-o', help='Enter the file name you wish to call the image (Default=PNG)', default='output.png')
    
    arguments= parser.parse_args()
    
-   this_chart = Greengraph(arguments.startPoint,arguments.endPoint)
-   this_data = this_chart.green_between(arguments.steps)
-   this_plot.plot(this_data)
-   #Add Annotations
-   this_plot.xlabel("Steps")
-   this_plot.ylabel("'Green Pixel Density'")
-   this_plot.title("Green Land Density: " + arguments.startPoint + " - " + arguments.endPoint)
-   #Save result and show user
-   this_plot.savefig(arguments.out)
-   this_plot.show()
+   chart(arguments.startPoint,arguments.endPoint,arguments.steps,arguments.out)
+   
