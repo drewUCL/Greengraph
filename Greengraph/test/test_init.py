797d3a8774fea90d5026523f59532f0b525471cc
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-10 17:27:44 +0000

    Added command line tests for better coverage

--- Greengraph/test/test_init.py
+++ Greengraph/test/test_init.py
@@ -3,7 +3,7 @@ import mock
 from ..__init__ import parser, chart, process
 from ..graph import Greengraph as GR
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_true
 from matplotlib import pyplot as py
 
 def test_command():
@@ -28,5 +28,11 @@ def test_chart(mock_show, mock_savefig, mock_plot, mock_green_between):
 	Description: A function to test if the charting function is in operation
 	'''
 	arguments = parser.parse_args(['--from','London','--to','Edinburgh','--steps','12','--out','LDN_EDN.png'])
-	chart(arguments.StartLocation,arguments.EndLocation,arguments.steps,arguments.Out)
+	chart(arguments.StartLocation,arguments.EndLocation,arguments.steps,arguments.out)
 	mock_green_between.assert_called_with(12)
+
+@mock.patch('Greengraph.__init__.parser.parse_args')
+@mock.patch('Greengraph.__init__.chart')
+def test_process(mock_parser, mock_chart):
+	process()
+	assert_true(mock_parser.called)
