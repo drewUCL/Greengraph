bbbcfaf454ce181785268b83d2030e6d4394736a
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-10 14:53:39 +0000

    Fixing indentation error in test_init.py

--- Greengraph/test/test_init.py
+++ Greengraph/test/test_init.py
@@ -7,12 +7,11 @@ def test_command():
 	Data: STATIC
 	'''
 	parser = ArgumentParser( prog="Greengraph", description = "Generate Green Density Chart Based on Two Points")
-   
-    parser.add_argument('--from', '-f', help = 'Enter a start location here such as London. [STRING]',dest='StartLocation', required=True)
-    parser.add_argument('--to',  '-t', help = 'Enter an end location here such as Oxford. [STRING]',dest='EndLocation', required=True) 
-    parser.add_argument('--steps', '-s', type = int, help = 'Enter the number of steps you wish to chart. [INT]', default = 10)
-    parser.add_argument('--out', '-o', help = 'Enter the file name you wish to call the image with extension (Default=.png) [STRING]', default = 'output.png')
-   
+	parser.add_argument('--from', '-f', help = 'Enter a start location here such as London. [STRING]',dest='StartLocation', required = True)
+	parser.add_argument('--to',  '-t', help = 'Enter an end location here such as Oxford. [STRING]',dest='EndLocation', required = True) 
+	parser.add_argument('--steps', '-s', type = int, help = 'Enter the number of steps you wish to chart. [INT]', default = 10)
+	parser.add_argument('--out', '-o', help = 'Enter the file name you wish to call the image with extension (Default=.png) [STRING]', default = 'output.png')
+	
 	commands = parser.parse_args(['--from','London','--to','Edinburgh','--steps',12,'--out','LDN_EDN.png'])
 	
 	assert_equal(commands.StartLocation,'London')
