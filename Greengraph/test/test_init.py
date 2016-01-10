292b985c45624be1ce047ed7a1c8e4e6341cad0a
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-10 17:04:28 +0000

    Added test for chart function

--- Greengraph/test/test_init.py
+++ Greengraph/test/test_init.py
@@ -1,5 +1,12 @@
+import mock
+
 from ..__init__ import parser
+from ..graph import Greengraph as GR
+
 from nose.tools import assert_equal
+from matplotlib import pyplot as py
+
+
 
 def test_command():
 	'''
@@ -12,3 +19,16 @@ def test_command():
 	assert_equal(commands.EndLocation,'Edinburgh')
 	assert_equal(commands.steps,12)
 	assert_equal(commands.out,'LDN_EDN.png')
+
+
+@mock.patch.object(py, 'show')
+@mock.patch.object(py, 'savefig')
+@mock.patch.object(py, 'plot')
+@mock.patch.object(GR, 'green_between')
+def test_chart():
+	'''
+	Description: A function to test if the charting function is in operation
+	'''
+	arguments = parser.parse_args(['--from','London','--to','Edinburgh','--steps','12','--out','LDN_EDN.png'])
+	chart(arguments.StartLocation,arguments.EndLocation,arguments.Steps,arguments.Out)
+	mock_green_between.assert_called_with(12)
