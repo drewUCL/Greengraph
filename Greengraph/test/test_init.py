a9e1c946282faaa60059d13915749d932abe989e
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-10 15:03:18 +0000

    adjusting command line for testing

--- Greengraph/test/test_init.py
+++ Greengraph/test/test_init.py
@@ -7,6 +7,7 @@ def test_command():
 	Data: STATIC
 	'''
 	parser = ArgumentParser( prog="Greengraph", description = "Generate Green Density Chart Based on Two Points")
+	
 	parser.add_argument('--from', '-f', help = 'Enter a start location here such as London. [STRING]',dest='StartLocation', required = True)
 	parser.add_argument('--to',  '-t', help = 'Enter an end location here such as Oxford. [STRING]',dest='EndLocation', required = True) 
 	parser.add_argument('--steps', '-s', type = int, help = 'Enter the number of steps you wish to chart. [INT]', default = 10)
@@ -15,6 +16,6 @@ def test_command():
 	commands = parser.parse_args(['--from','London','--to','Edinburgh','--steps',12,'--out','LDN_EDN.png'])
 	
 	assert_equal(commands.StartLocation,'London')
-	assert_equal(commands.EndLocation,'London')
+	assert_equal(commands.EndLocation,'Edinburgh')
 	assert_equal(commands.steps,12)
 	assert_equal(commands.out,'LDN_EDN.png')
