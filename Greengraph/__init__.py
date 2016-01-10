1c20b5972eaf6e84c633a1a9701d290ec0a280d8
Andrew Mann <drewmann47@yahoo.co.uk>
2016-01-10 15:22:53 +0000

    Tweeked the command line structure and testing

--- Greengraph/__init__.py
+++ Greengraph/__init__.py
@@ -2,6 +2,15 @@ from graph import Greengraph
 from argparse import ArgumentParser
 from matplotlib import pyplot as this_plot
 
+
+#Global Variables - These have been moved from the process function to glabal variabes to allow me to test the command line
+parser = ArgumentParser( prog="Greengraph", description = "Generate Green Density Chart Based on Two Points")   
+parser.add_argument('--from', '-f', help = 'Enter a start location here such as London. [STRING]',dest='StartLocation', required=True)
+parser.add_argument('--to',  '-t', help = 'Enter an end location here such as Oxford. [STRING]',dest='EndLocation', required=True) 
+parser.add_argument('--steps', '-s', type = int, help = 'Enter the number of steps you wish to chart. [INT]', default = 10)
+parser.add_argument('--out', '-o', help = 'Enter the file name you wish to call the image with extension (Default=.png) [STRING]', default = 'output.png')
+
+ 
 def chart(start, end, steps, out):
    this_chart = Greengraph(start,end)
    this_data = this_chart.green_between(steps)
@@ -20,14 +29,5 @@ def process():
        This Function Then Plots The Results Where The Package Was Called From
 	   For Example, If It Was Called From The Desktop, The Resulting Chart Would Be Saved On The Desktop For The User.
    '''
-
-   parser = ArgumentParser( prog="Greengraph", description = "Generate Green Density Chart Based on Two Points")
-   
-   parser.add_argument('--from', '-f', help = 'Enter a start location here such as London. [STRING]',dest='StartLocation', required=True)
-   parser.add_argument('--to',  '-t', help = 'Enter an end location here such as Oxford. [STRING]',dest='EndLocation', required=True) 
-   parser.add_argument('--steps', '-s', type = int, help = 'Enter the number of steps you wish to chart. [INT]', default = 10)
-   parser.add_argument('--out', '-o', help = 'Enter the file name you wish to call the image with extension (Default=.png) [STRING]', default = 'output.png')
-   
    arguments= parser.parse_args()
-   
    chart(arguments.StartLocation,arguments.EndLocation,arguments.Steps,arguments.Out)
--- Greengraph/test/test_init.py
+++ Greengraph/test/test_init.py
@@ -6,15 +6,8 @@ def test_command():
 	Description: A function to test the command line arguments
 	Data: STATIC
 	'''
-	parser = ArgumentParser( prog="Greengraph", description = "Generate Green Density Chart Based on Two Points")
-	
-	parser.add_argument('--from', '-f', help = 'Enter a start location here such as London. [STRING]',dest='StartLocation', required = True)
-	parser.add_argument('--to',  '-t', help = 'Enter an end location here such as Oxford. [STRING]',dest='EndLocation', required = True) 
-	parser.add_argument('--steps', '-s', type = int, help = 'Enter the number of steps you wish to chart. [INT]', default = 10)
-	parser.add_argument('--out', '-o', help = 'Enter the file name you wish to call the image with extension (Default=.png) [STRING]', default = 'output.png')
-	
-	commands = parser.parse_args(['--from','London','--to','Edinburgh','--steps',12,'--out','LDN_EDN.png'])
-	
+	commands = parser.parse_args(['--from','London','--to','Edinburgh','--steps','12','--out','LDN_EDN.png'])
+	#Test Each Command
 	assert_equal(commands.StartLocation,'London')
 	assert_equal(commands.EndLocation,'Edinburgh')
 	assert_equal(commands.steps,12)
